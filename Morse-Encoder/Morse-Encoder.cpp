1ab5b02b5cdd1ef1ad4f0f5a214ea69b3bd0c202
Billy McAllister <sol_subzero@hotmail.com>
2018-12-14 15:57:08 -0600

    Okay....So..this is an Object Oriented approach. Not sure if it is more efficient, but It was a way for me to learn more about OOP.

--- Morse-Encoder/Morse-Encoder.cpp
+++ Morse-Encoder/Morse-Encoder.cpp
@@ -3,66 +3,70 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <algorithm>
 
-void MorseTranslator(std::vector<std::string>& morse);
-
-int main()
-{
-	std::vector<std::string> morse;
-	std::string input;
-
-	std::cout << "Please enter any word/words to be translated into morse:\n ";
-	std::getline(std::cin, input);
-
-	morse.push_back(input);
-	MorseTranslator(morse);
-
-
-	std::cout << " " << morse[0];
-
-}
-
-void MorseTranslator(std::vector<std::string>& morse) {
-	std::map<char, std::string> Morse;
-
-	Morse['a'] = ".-";
-	Morse['b'] = "-...";
-	Morse['c'] = "-.-.";
-	Morse['d'] = "-..";
-	Morse['e'] = ".";
-	Morse['f'] = "..-.";
-	Morse['g'] = "--.";
-	Morse['h'] = "....";
-	Morse['i'] = "..";
-	Morse['j'] = ".---";
-	Morse['k'] = "-.-";
-	Morse['l'] = ".-..";
-	Morse['m'] = "--";
-	Morse['n'] = "-.";
-	Morse['o'] = "---";
-	Morse['p'] = ".--.";
-	Morse['q'] = "--.-";
-	Morse['r'] = ".-.";
-	Morse['s'] = "...";
-	Morse['t'] = "-";
-	Morse['u'] = "..-";
-	Morse['v'] = "...-";
-	Morse['w'] = ".--";
-	Morse['x'] = "-..-";
-	Morse['y'] = "-.--";
-	Morse['z'] = "--..";
-	Morse[' '] = "/";
-
+class Morse {
+private:
 	std::string value = "";
 	std::vector<std::string> dummyvec;
 
-	for (std::string word : morse) {
-		for (char c : word) {
-			value.append(Morse[c]);
+public:
+
+	std::vector<std::string> morse;
+	std::string input;
+
+	void Translate(std::vector<std::string>& morse) {
+		std::map<const char, std::string> Morse;
+
+		Morse['a'] = ".-";
+		Morse['b'] = "-...";
+		Morse['c'] = "-.-.";
+		Morse['d'] = "-..";
+		Morse['e'] = ".";
+		Morse['f'] = "..-.";
+		Morse['g'] = "--.";
+		Morse['h'] = "....";
+		Morse['i'] = "..";
+		Morse['j'] = ".---";
+		Morse['k'] = "-.-";
+		Morse['l'] = ".-..";
+		Morse['m'] = "--";
+		Morse['n'] = "-.";
+		Morse['o'] = "---";
+		Morse['p'] = ".--.";
+		Morse['q'] = "--.-";
+		Morse['r'] = ".-.";
+		Morse['s'] = "...";
+		Morse['t'] = "-";
+		Morse['u'] = "..-";
+		Morse['v'] = "...-";
+		Morse['w'] = ".--";
+		Morse['x'] = "-..-";
+		Morse['y'] = "-.--";
+		Morse['z'] = "--..";
+		Morse[' '] = "/";
+
+		for (std::string word : morse) {
+			for (char c : word) {
+				value.append(Morse[c]);
+			}
+			dummyvec.push_back(value);
+			value = "";
 		}
-		dummyvec.push_back(value);
-		value = "";
+
+		morse.swap(dummyvec);
 	}
-	morse.swap(dummyvec);
+};
+
+int main()
+{
+	Morse Coder;
+
+	std::cout << "Please enter any word/words to be translated into morse:\n ";
+	std::getline(std::cin, Coder.input);
+
+	Coder.morse.push_back(Coder.input);
+	
+	Coder.Translate(Coder.morse);
+
+	std::cout << " " << Coder.morse[0];
 }
